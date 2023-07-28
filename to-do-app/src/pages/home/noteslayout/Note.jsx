5d9cf047e55214541a8f3b5646cfe5db2f2a2026
Raashid <muhammedraashid@colanonline.com>
2023-07-28 12:57:30 +0530

    Build issue resolved

--- to-do-app/src/pages/home/noteslayout/Note.jsx
+++ to-do-app/src/pages/home/noteslayout/Note.jsx
@@ -1,92 +1,94 @@
-import React from 'react'
+import React from "react";
 import DeleteIcon from "@mui/icons-material/Delete";
 import { IconButton } from "@mui/material";
-import { useRef } from 'react';
+import { useRef } from "react";
 
 let timer = 500,
   timeout;
 const Note = (props) => {
-    const {UpdateText}= props
-    const textareaRef = useRef(null);
-  
-    function adjustTextAreaHeight() {
-      const textarea = textareaRef.current;
-      if (textarea) {
-        textarea.style.height = "auto"; // Reset the height to auto to recalculate the new height based on content
-        textarea.style.height = `${textarea.scrollHeight}px`; // Set the height to fit the content
-      }
-    }
-  
-    const formatDate = (value) => {
-      console.log("Received date value:", value);
-      if (!value) return "";
-  
-      const date = new Date(value);
-      const monthNames = [
-        "jan",
-        "feb",
-        "march",
-        "april",
-        "may",
-        "jun",
-        "jul",
-        "aug",
-        "sept",
-        "oct",
-        "nov",
-        "dec",
-      ];
-  
-      let hrs = date.getHours();
-      let amPm = hrs > 12 ? "PM " : "AM";
-      hrs = hrs ? hrs : "12";
-      hrs = hrs > 12 ? (hrs = 24 - hrs) : hrs;
-  
-      let min = date.getMinutes();
-      min = min < 10 ? "0" + min : min;
-  
-      let day = date.getDate();
-      const month = monthNames[date.getMonth()];
-      return `${hrs}:${min} ${amPm}  ${day} ${month}`;
-    };
-  
-    const debounce = (func) => {
-      clearTimeout(timeout);
-      timeout = setTimeout(func, timer);
-    };
-  
-    const UpdateText1 = (text, id) => {
-      debounce(() => UpdateText(text, id));
-    };
-  return (
-    <div className="note" style={{ backgroundColor: props.note.color }}>
-    <textarea
-      //  onChange={(event)=>{}}
-      className="note_text"
-      defaultValue={props.note.text}
-      style={{ color: "white" }}
-      ref={textareaRef} // Use the ref to get a reference to the textarea
-      onChange={(event) => {
-        adjustTextAreaHeight(event);
-        UpdateText1(event.target.value, props.note.id);
-      }}
-    ></textarea>
-    <div className="note-footer">
-      <p style={{ color: "white" }}> {formatDate(props.note.time)}</p>
+  const { UpdateText } = props;
+  const textareaRef = useRef(null);
 
-      <div className="footer-delte-icon">
-        <IconButton
-          aria-label="delete"
-          onClick={() => {
-            props.deleteNote(props.note.id);
-          }}
-        >
-          <DeleteIcon sx={{ color: "white" }} />
-        </IconButton>
+  function adjustTextAreaHeight() {
+    const textarea = textareaRef.current;
+    if (textarea) {
+      textarea.style.height = "auto"; // Reset the height to auto to recalculate the new height based on content
+      textarea.style.height = `${textarea.scrollHeight}px`; // Set the height to fit the content
+    }
+  }
+
+  const formatDate = (value) => {
+    console.log("Received date value:", value);
+    if (!value) return "";
+
+    const date = new Date(value);
+    const monthNames = [
+      "jan",
+      "feb",
+      "march",
+      "april",
+      "may",
+      "jun",
+      "jul",
+      "aug",
+      "sept",
+      "oct",
+      "nov",
+      "dec",
+    ];
+
+    let hrs = date.getHours();
+    let amPm = hrs > 12 ? "PM " : "AM";
+    hrs = hrs ? hrs : "12";
+    hrs = hrs > 12 ? (hrs = 24 - hrs) : hrs;
+
+    let min = date.getMinutes();
+    min = min < 10 ? "0" + min : min;
+
+    let day = date.getDate();
+    const month = monthNames[date.getMonth()];
+    return `${hrs}:${min} ${amPm}  ${day} ${month}`;
+  };
+
+  const debounce = (func) => {
+    clearTimeout(timeout);
+    timeout = setTimeout(func, timer);
+  };
+
+  const UpdateText1 = (text, id) => {
+    debounce(() => UpdateText(text, id));
+  };
+
+  const NoteColor = props.note.color || "#ffffff";
+  return (
+    <div className="note" style={{ backgroundColor: NoteColor }}>
+      <textarea
+        //  onChange={(event)=>{}}
+        className="note_text"
+        defaultValue={props.note.text}
+        style={{ color: "white" }}
+        ref={textareaRef} // Use the ref to get a reference to the textarea
+        onChange={(event) => {
+          adjustTextAreaHeight(event);
+          UpdateText1(event.target.value, props.note.id);
+        }}
+      ></textarea>
+      <div className="note-footer">
+        <p style={{ color: "white" }}> {formatDate(props.note.time)}</p>
+
+        <div className="footer-delte-icon">
+          <IconButton
+            aria-label="delete"
+            onClick={() => {
+              props.deleteNote(props.note.id);
+            }}
+          >
+            <DeleteIcon sx={{ color: "white" }} />
+          </IconButton>
+        </div>
       </div>
     </div>
-  </div>
-  )
-}
+  );
+};
 
-export default Note
+export default Note;
--- to-do-app/src/pages/home/noteslayout/NoteContainer.jsx
+++ to-do-app/src/pages/home/noteslayout/NoteContainer.jsx
@@ -1,27 +1,54 @@
-import React from 'react'
-import Note from './Note'
+import React from "react";
+import Note from "./Note";
+const reverseArray = (arr) => {
+  if (!arr || arr.length === 0) {
+    return []; // Return an empty array if input is undefined or empty
+  }
 
+  const array = [];
+  for (let i = arr.length - 1; i >= 0; --i) {
+    array.push(arr[i]);
+  }
+  return array;
+};
 const NoteContainer = (props) => {
-    const reverseArray =(arr) =>{
-        const array=[]
-       for(let i=arr.length -1; i>=0;--i){
-        array.push(arr[i]) 
-       }
-       return array;
-     }
-   
-     const  notes=reverseArray(props.notes)
-  return (
-    <div className="note-container" style={{color:"white"}}>
-    <h2>Notes</h2>
-    <div className="note-container-notes custom-scroll">
-      { notes?.length >0?(   notes.map((item, i) => {
-         console.log(item);
-         return <Note key={item.id} note={item} deleteNote={props.deleteNote } UpdateText={props.UpdateText}  />;
-          })): <h3>No notes here</h3> }
-    </div>
-  </div>
-  )
-}
+  // const reverseArray = (arr) => {
+  //   if (!arr || arr.length === 0) {
+  //     return []; // Return an empty array if input is undefined or empty
+  //   }
 
-export default NoteContainer
+  //   const array = [];
+  //   for (let i = arr.length - 1; i >= 0; --i) {
+  //     array.push(arr[i]);
+  //   }
+  //   return array;
+  // };
+
+  // const notes = reverseArray(props.notes);
+  const notes = reverseArray(props.notes);
+
+  return (
+    <div className="note-container" style={{ color: "white" }}>
+      <h2>Notes</h2>
+      <div className="note-container-notes custom-scroll">
+        {notes?.length > 0 ? (
+          notes.map((item, i) => {
+            console.log(item);
+            return (
+              <Note
+                key={item.id}
+                note={item}
+                deleteNote={props.deleteNote}
+                UpdateText={props.UpdateText}
+              />
+            );
+          })
+        ) : (
+          <h3>No notes here</h3>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default NoteContainer;
